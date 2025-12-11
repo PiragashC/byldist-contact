c1717a5c7b45c3dbb3bc17e6e96385cad4d860ef
Piragash <pragashconstantine13@gmail.com>
2025-12-11 15:47:15 +0530

    changes

--- ByldistContactUs/settings.py
+++ ByldistContactUs/settings.py
@@ -146,6 +146,7 @@ EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "true").lower() == "true"
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER or "no-reply@example.com")
+EMAIL_TIMEOUT = int(os.getenv("EMAIL_TIMEOUT", "20"))  # Timeout in seconds
 
 # The recipient that should receive contact form submissions.
 CONTACT_US_RECIPIENT = os.getenv("CONTACT_US_RECIPIENT", "")
